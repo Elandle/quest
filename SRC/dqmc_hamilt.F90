fb148a1069a38be19c0be210a4fc4fd095f49be2
Meromorphics <144414492+Meromorphics@users.noreply.github.com>
2024-02-29 04:29:07 -0800

    Fixed spacing

--- SRC/dqmc_hamilt.F90
+++ SRC/dqmc_hamilt.F90
@@ -98,7 +98,7 @@ module DQMC_HAMILT
             nullify(tcfg)
 
             if (.not. lattice%constructed)       stop 'Need to construct lattice before building Hamiltonian'
-            if (.not. recip_lattice%initialized) stop'Need to initialize recip_lattice before building Hamiltonian'
+            if (.not. recip_lattice%initialized) stop 'Need to initialize recip_lattice before building Hamiltonian'
 
             ! Set local alias
             natom  = lattice%natom
@@ -243,7 +243,7 @@ module DQMC_HAMILT
             integer, pointer :: tneig(:,:),Uneig(:,:),Jneig(:,:)
             integer, pointer :: tsite(:),Usite(:),Jsite(:)
      
-            if(.not. hamilt%constructed) stop'Hamiltonian needs to be constructed before neig can be found'
+            if(.not. hamilt%constructed) stop 'Hamiltonian needs to be constructed before neig can be found'
     
             n = size(hamilt%hopup, 1)
 
